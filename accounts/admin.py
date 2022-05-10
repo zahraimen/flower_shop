513152560c41c19841f4bec1bef77511bdc459b6
zahra imen <zahraimen91@gmail.com>
2022-05-10 18:24:55 +0430

    create 2 forms as name as CustomUserCreationForm and CustomUserChangeForm in accounts app and create custom user admin then connected the forms to the admin and models in register

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, UserChangeForm
+from .models import CustomUser
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
