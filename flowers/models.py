26cb308a566f05365e0abe09f37c65facfa8ed35
zahra imen <zahraimen91@gmail.com>
2022-05-20 18:30:55 +0430

    create get_absolute_url for unique id

--- flowers/models.py
+++ flowers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 # Create your models here.
@@ -10,3 +11,6 @@ class Flower(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('flower_detail', args=[self.id])
