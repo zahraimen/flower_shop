303328a25faae7592b0adcb1b07e4894e71b78ad
zahra imen <zahraimen91@gmail.com>
2022-05-20 17:55:12 +0430

    finally migrate all of the models

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,4 @@ class CustomUser(AbstractUser):
     city = models.CharField(max_length=200)
 
 
-class Seller(models.Model):
-    seller = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    phone = models.PositiveIntegerField(max_length=11)
+
--- flowers/models.py
+++ flowers/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Flower(models.Model):
     title = models.CharField(max_length=225)
-    flower_seller = models.ForeignKey('accounts.Seller', on_delete=models.CASCADE)
+    seller = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
     description = models.TextField()
     price = models.IntegerField()
 
