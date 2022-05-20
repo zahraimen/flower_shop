c47b7a011dbc2e3b0889f003dd9031389ae9618d
zahra imen <zahraimen91@gmail.com>
2022-05-20 19:13:42 +0430

    create Flower Create view

---
+++ flowers/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Flower
+
+
+class FlowerCreation(forms.ModelForm):
+    class Meta:
+        model = Flower
+        fields = ('title', 'seller', 'description', 'price',)
--- flowers/views.py
+++ flowers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 # Create your views here.
 from .models import Flower
+from .forms import FlowerCreation
 
 
 class FlowerListView(generic.ListView):
@@ -13,3 +14,8 @@ class FlowerListView(generic.ListView):
 class FlowerDetailView(generic.DetailView):
     model = Flower
     template_name = 'flowers/flower_detail.html'
+
+
+class FlowerCreateView(generic.CreateView):
+    form_class = FlowerCreation
+    template_name = 'flowers/flower_create.html'
